55203ed815ca20b3265c05d39635a1e47dc7268f
TechDomani <natalie.dillon@gmail.com>
2021-04-28 17:47:47 +0100

    Initial Map added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RentalYield from './Component/RentalYield';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <RentalYield />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/RentalYield.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Map from './Map/Map'
+import axios from 'axios';
+
+function RentalYield() {
+
+    const [centerMap, setCenterMap] = useState<[number, number]>([51.505, -0.09]);
+    const [postCode, setPostCode] = useState<string>('e112dz');
+
+    useEffect(() => {
+        axios.get("https://api.postcodes.io/postcodes/e112dz")
+            .then((result) => {
+                console.log(result);
+                let latitude = result.data.result.latitude;
+                let longitude = result.data.result.longitude;
+                console.log(latitude);
+                console.log(longitude);
+                setCenterMap([latitude, longitude])
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, [postCode]
+    ); 
+
+
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h1>I will show rental yield</h1>
+            <Map latitude={centerMap[0]} longitude={centerMap[1]}/>
+        </div>
+    );
+}
+
+export default RentalYield;
