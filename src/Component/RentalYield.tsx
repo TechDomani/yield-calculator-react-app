6cc815d92bcf0e68d5c7a61d30fc432610b00124
TechDomani <natalie.dillon@gmail.com>
2021-04-29 11:32:58 +0100

    React Leaflet removed

--- src/Component/RentalYield.tsx
+++ src/Component/RentalYield.tsx
@@ -7,20 +7,12 @@ function RentalYield() {
     const [position, setPosition] = useState<[number, number]>([59.91174337077401, 10.750425582038146]);
     const [postCode, setPostCode] = useState<string>('e112dz');
 
-    useEffect(() => {
-        console.log('Position has changed:' + position);
-    }, position);
-
     const search = () => {
         axios.get("https://api.postcodes.io/postcodes/" + postCode)
         .then((result) => {
-            console.log(result);
             let latitude = result.data.result.latitude;
             let longitude = result.data.result.longitude;
-            console.log(latitude);
-            console.log([latitude, longitude]);
             setPosition([latitude, longitude])
-            console.log(position)
         })
         .catch((err) => {
             console.log(err);
--- src/Component/YieldMap/YieldMap.tsx
+++ src/Component/YieldMap/YieldMap.tsx
@@ -1,55 +1,40 @@
 import "leaflet/dist/leaflet.css";
 import L, { LatLngExpression } from 'leaflet';
-//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import React, { MutableRefObject, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './YieldMap.css';
 
 interface YieldMapProps {
-   latitude: number,
-   longitude: number
+  latitude: number,
+  longitude: number
 }
 
-function YieldMap({latitude, longitude}: YieldMapProps) {
+function YieldMap({ latitude, longitude }: YieldMapProps) {
 
-    console.log('Yield map props are ' + latitude + ' ' + longitude)
-    
-    const mapRef = useRef<L.Map | null>(null);
-    const zoom : number = 15;
-   // const [leafletMap
+  console.log('Yield map props are ' + latitude + ' ' + longitude)
 
-    useEffect(() => {
-      const position : LatLngExpression = [latitude, longitude];
-    console.log(`Map ref is ${mapRef}`);
-    if (!mapRef.current){
-    mapRef.current =
-      L.map('map', {
-        center: position,
-        zoom: zoom,
-        layers: [
-          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution:
-              '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          }),
-        ]
-      });
+  const mapRef = useRef<L.Map | null>(null);
+  const zoom: number = 15;
+
+  useEffect(() => {
+    const position: LatLngExpression = [latitude, longitude];
+    if (!mapRef.current) {
+      mapRef.current =
+        L.map('map', {
+          center: position,
+          zoom: zoom,
+          layers: [
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+              attribution:
+                '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }),
+          ]
+        });
     } else {
       mapRef.current.setView(position);
     }
-      console.log(`Map ref is ${mapRef}`);
-    }, [latitude, longitude]);
+  }, [latitude, longitude]);
 
-    return <div id="map"></div>
-
-   /*  return (<MapContainer className="map-display" center={position} zoom={zoom} scrollWheelZoom={false}>
-            <TileLayer
-            attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {
-                // Placeholder, we'll put our markers here
-            }
-        </MapContainer>
-    ); */
+  return (<div id="map"></div>);
 }
 
 export default YieldMap;
